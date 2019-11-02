73954d76605bdd43d11c1b3f696d98a633c914c9
ygzhang0103 <57283031+ygzhang0103@users.noreply.github.com>
2019-11-03 07:14:49 +0800

    Add files via upload

---
+++ sumbd.py
@@ -0,0 +1,20 @@
+__author__ = 'ygzhang'
+
+def bounds_of_rotations(x, c):
+        n = len(x)
+        Sum = []
+        m = 0
+        for m in range(n):
+                S = 0.0
+                i = 0
+                for i in range(n):
+                        if i+m < n:
+                                S = S+x[i+m]*c**i
+                        else:
+                                S = S+x[i+m-n]*c**i
+                        i =+ 1
+                Sum.append(S)
+                m =+ 1
+        Max = max(Sum)
+        Min = min(Sum)
+        print((Min, Max))
