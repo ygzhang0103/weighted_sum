12024476f4c32f8014ec5bbae100c1bf60ddb717
ygzhang0103 <57283031+ygzhang0103@users.noreply.github.com>
2019-11-04 00:26:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from mycode.sumbd import bounds_of_rotations
 
-if (sumbd.bounds_of_rotations([2, 5, 6, 8], 0.8) == (13.936,16.24):
+if (bounds_of_rotations([2, 5, 6, 8], 0.8) == (13.936,16.24):
     print('Passed')
 # Return will be (13.936, 16.24) which is just what we want.
