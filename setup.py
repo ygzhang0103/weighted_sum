2edf5805d4a03d0583c061deba483eb2591267b6
ygzhang0103 <57283031+ygzhang0103@users.noreply.github.com>
2019-11-03 23:35:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="weighted_sum_ygzhang0103", 
+    name="weighted_sum_bound_ygzhang0103", 
     version="0.0.1",
     author="Yuge Zhang",
     author_email="ygzhang0103@gmail.com",
