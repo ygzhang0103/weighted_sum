aa5d16e54941987d36e9e30b760c1e325bb8adbe
ygzhang0103 <57283031+ygzhang0103@users.noreply.github.com>
2019-11-03 21:14:24 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weighted_sum-ygzhang0103", 
+    version="0.0.1",
+    author="Yuge Zhang",
+    author_email="ygzhang0103@gmail.com",
+    description="A method to get lower and upper bounds of weighted sum",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
